1b6b8678cac34496e17a3b475e9034a2d18bd0f4
Azathoth <139758403+Az4thoth@users.noreply.github.com>
2023-11-17 00:24:30 +0330

    Update and rename MyToken.sol to token.sol

--- token.sol
+++ token.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MyToken is ERC20 {
+contract trashToken is ERC20 {
     constructor() ERC20("RandomToken", "Nise") {
-        _mint(msg.sender,1000000000000000000000000);
+        _mint(msg.sender,100000000000000000000000);
     }
 }
